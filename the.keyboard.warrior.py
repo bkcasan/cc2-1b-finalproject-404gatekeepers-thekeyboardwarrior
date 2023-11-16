6f35c7575e4e9a4c28c68a321c0e0af70bb0af7b
aevrivera6244 <145944226+aevrivera6244@users.noreply.github.com>
2023-11-16 23:20:03 +0800

    the.keyboard.warrior.py

--- the.keyboard.warrior.py
+++ the.keyboard.warrior.py
@@ -3,27 +3,149 @@ import time
 import random
 
 def start_game():
-    print("""""With your passion for the legend of the keyboard, you venture to the desert to acquire the lost remaining symbol relics that will complete the keyboard. The symbols !,$,&,* and ~.
-
-Your companion, Jebrael, helped you to get to the location because only you 1 were the last person in your bloodline who has the capabilities to take hold of them and place them to the right location.
-
-On the last leg, you finally acquire the “~” symbol. However the whole place started shaking.
-""""")
+    print("Welcome to the Maze Escape Game!")
     player_name = input("Enter your username: ")
     input("Press Enter to begin...")
-    print(f"{player_name}, you find yourself trapped in a mysterious maze. Your goal is to reach the exit.")
+    print(f"""With your passion for the legend of the keyboard, you venture to the desert to acquire the lost remaining symbol relics that will complete the keyboard. The symbols !,$,&,* and ~.
+
+Your companion, Jebrael, helped you to get to the location because only you, {player_name}, were the last person in your bloodline who has the capabilities to take hold of them and place them to the right location.
+
+On the last leg, you finally acquire the “~” symbol. However the whole place started shaking.""")
 
     simulate_maze_escape()
 
 def credits():
-    print("Game created by Your Name")
+    print("""Game created by:
+Gerryle Martinez
+Yasmien Gayao
+Adrian Rivera
+Ivan Corpuz""")
 
 def simulate_maze_escape():
     print("Simulating maze escape...")
     time.sleep(2)
+    Maze1 = """  
+       ████████████████████████████
+          █                 █     █
+    █  ████  █████████████  █  ████
+    █  █        █  █  █     █     █
+    █  █  ███████  █  ████  ████  █
+    █           █        █        █
+    ████  █  ████  ████  ███████  █
+    █     █     █  █  █  █        █
+    █  ████  ████  █  █  █  █  ████
+    █  █  █        █     █  █  █  █
+    █  █  ███████  ██████████  █  █
+    █        █  █     █     █     █
+    █  █  ████  ████  █  ███████  █
+    █  █     █           █  █     █
+    █  █████████████  ████  ████  █
+    █              █        █  █  █
+    ███████  ████  ████  ████  ████
+    █        █  █  █  █  █  █  █  █
+    █  ███████  █  █  █  █  █  █  █
+    █     █           █           
+    ████████████████████████████  """
+
+    Maze2 = """  
+       ████████████████████████████
+             █                 █  █
+    ███████  ██████████  ████  █  █
+    █  █     █           █     █  █
+    █  █  ███████  ███████  ████  █
+    █  █  █     █  █     █        █
+    █  █  █  ████  █  ███████  █  █
+    █           █  █           █  █
+    █  █  ███████  █  █  ████  █  █
+    █  █           █  █  █     █  █
+    █  █  █  ███████  ██████████  █
+    █  █  █  █  █        █     █  █
+    █  █  ████  █  ██████████  ████
+    █  █  █     █  █              █
+    █  ████  ████  ███████  ████  █
+    █     █                 █     █
+    ████  ████  █  ████  ████  █  █
+    █        █  █  █  █  █     █  █
+    ████  ████  █  █  █  █  █  ████
+    █        █  █     █  █  █     
+    ████████████████████████████  """
+
+    Maze3 = """   
+       ████████████████████████████
+             █     █        █     █
+    ████  ███████  ████  █  █  ████
+    █     █  █     █  █  █  █     █
+    █  ████  █  █  █  █  █  ████  █
+    █        █  █  █     █     █  █
+    █  █  █  ████  █  ██████████  █
+    █  █  █     █        █  █  █  █
+    ████  ████  █  ████  █  █  █  █
+    █        █     █     █     █  █
+    ████  █████████████  █  █  █  █
+    █     █  █  █  █  █     █     █
+    █  ████  █  █  █  █  ███████  █
+    █  █  █  █     █  █  █        █
+    █  █  █  █  ████  ███████  █  █
+    █                    █     █  █
+    █  █  █  ██████████  █  ████  █
+    █  █  █     █        █  █     █
+    █  █  ███████  ████  ████  █  █
+    █  █     █     █        █  █  
+    ████████████████████████████  """
+
+    Maze4 = """
+       ████████████████████████████
+          █     █                 █
+    █  ████  ████  ████  ████  ████
+    █  █  █     █     █  █     █  █
+    █  █  █  █████████████  █  █  █
+    █  █     █  █     █  █  █  █  █
+    █  █  █  █  ████  █  ████  █  █
+    █     █           █        █  █
+    █  ███████  █  █  █  █  █  █  █
+    █     █  █  █  █     █  █     █
+    ████  █  ███████  █  ████  ████
+    █  █        █  █  █  █  █  █  █
+    █  █  ███████  ███████  █  █  █
+    █  █  █     █     █           █
+    █  █  █  ████  ███████  █  ████
+    █        █           █  █  █  █
+    █  ████  ████  ████  █  ████  █
+    █     █  █     █           █  █
+    ███████  ████  ███████  ████  █
+    █           █  █              
+    ████████████████████████████  """
+
+    Maze5 = """   
+       ████████████████████████████
+          █     █                 █
+    █  █  █  ███████  ███████  █  █
+    █  █  █        █  █  █     █  █
+    ████  █  ███████  █  ████  ████
+    █     █              █        █
+    ████  █  █████████████  █  ████
+    █     █     █        █  █     █
+    ████  █  █  █  ███████  █  █  █
+    █     █  █     █  █     █  █  █
+    █  █  █  ████  █  ███████  ████
+    █  █     █        █  █  █  █  █
+    ███████  █  █  █  █  █  ████  █
+    █        █  █  █     █        █
+    █  ████  █  ███████  █  █  █  █
+    █  █  █  █  █     █     █  █  █
+    ████  █  █  █  ██████████  ████
+    █  █     █  █     █           █
+    █  █  ██████████  █  ████  █  █
+    █        █              █  █  
+    ████████████████████████████  """
+
+    Mazetotal= [Maze1, Maze2, Maze3, Maze4, Maze5]
+
+    rand = random.choice(Mazetotal)
+    print(rand)
     print("Congratulations! You have escaped the maze.")
     sys.exit()
-
+    
 while True:
     print("\n1. Start Game")
     print("2. Credits")
